0ccdb04dd24038527f73eed624a2a4365675c56b
ANNIE CROTEAU <anniecroteauc@gmail.com>
2024-05-29 16:28:10 +0200

    all projects and video feature added

--- src/components/ProjectCardLeft.jsx
+++ src/components/ProjectCardLeft.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { useInView } from "react-intersection-observer";
 
+import { useLanguageContext } from "../contexts/LanguageContext";
 import styles from "../styles/ProjectCardLeft.module.scss";
 import { FiGithub } from "react-icons/fi";
 import { FiExternalLink } from "react-icons/fi";
 
 function ProjectCardLeft({
   photo,
+  type,
   title,
-  description,
+  descriptionEnglish,
+  descriptionFrench,
   technologies,
   githubLink,
   siteLink,
 }) {
   const { ref: projectRef, inView: projectIsVisible } = useInView();
-
+  const { language } = useLanguageContext();
   return (
     <section ref={projectRef} className={styles.projectCardLeft}>
       <div
@@ -23,11 +26,16 @@ function ProjectCardLeft({
         }`}
       >
         <div className={styles.imageWrapper}>
-          <img
-            src={photo}
-            className={styles.photo}
-            alt={`Homepage of ${title} website`}
-          />
+          {type === "photo" ? (
+            <a href={siteLink}>
+              <img src={photo} className={styles.photo} />
+            </a>
+          ) : (
+            <video controls className={styles.photo}>
+              <source src={photo} type="video/mp4" />
+              Your browser does not support the video tag
+            </video>
+          )}
         </div>
       </div>
 
@@ -37,7 +45,9 @@ function ProjectCardLeft({
         }`}
       >
         <h1 className={styles.title}>{title}</h1>
-        <div className={styles.description}>{description}</div>
+        <div className={styles.description}>
+          {language === "english" ? descriptionEnglish : descriptionFrench}
+        </div>
         <div className={styles.technoBox}>
           {technologies.map((techno) => (
             <p className={styles.techno}>{techno}</p>
@@ -47,9 +57,13 @@ function ProjectCardLeft({
           <a className={styles.link} href={githubLink}>
             <FiGithub size={25} />
           </a>
-          <a className={styles.link} href={siteLink}>
-            <FiExternalLink size={28} />
-          </a>
+          {siteLink ? (
+            <a className={styles.link} href={siteLink}>
+              <FiExternalLink size={28} />
+            </a>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </section>
